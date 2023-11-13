d49847f1ca4cd54c9a85ce3b8ee68a88d6445800
Immii-Ahmed <imranahmedrajpoot@gmail.com>
2023-11-13 00:47:13 -0800

    I Changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Home from './pages/Home'
 
-export function App() {
+export default function App() {
   return (
     <div className="App">
-      <Home name={ 'Wajahat' } />
+      <Home name={ 'Imran' } />
     </div>
   );
 }
