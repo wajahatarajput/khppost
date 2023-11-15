9be284edc9b65c050e9eab5a71e4d419d813b5a9
mujeebali2703 <mujeeb.ali2703@gmail.com>
2023-11-15 15:39:43 +0500

    changes

--- src/feature/RegisterFeature.js
+++ src/feature/RegisterFeature.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from '../components/Card/Card';
 import RegisterForm from '../components/RegisterForm/RegisterForm';
 
-
 const RegisterFeature = () => {
   return (
     <Card heading="Register On KHP Social Media">
