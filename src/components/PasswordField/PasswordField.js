2d8c84141546e7a1c6e3e08b143af93dd4791ae9
mujeebali2703 <mujeeb.ali2703@gmail.com>
2023-11-15 15:36:19 +0500

    changes

---
+++ src/components/PasswordField/PasswordField.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import "./style.css"
+
+const PasswordField = (props) => {
+    const {placeholder, handlePasswordChange} = props;
+
+const [showPassword,setShowPassword] = useState('password');
+
+const toggleShowPassword = ()=>{
+    setShowPassword(showPassword === 'password' ? 'text' : 'password');
+}
+
+  return (
+    <div className='div-border my-2 form-control d-flex justify-content-between align-items-center border border-rounded'>
+        <input onChange={(e)=>handlePasswordChange(e.target.value)} className='remove-shadow shadow-none border border-0' type={showPassword} placeholder={placeholder}/>
+        <i class="fa fa-eye" onClick={toggleShowPassword}></i>
+    </div>
+  )
+}
+
+export default PasswordField
