a9b8f0695d49a323758578ed139f3dcc1394cd62
mujeebali2703 <mujeeb.ali2703@gmail.com>
2023-11-27 16:03:10 +0500

    as

--- src/feature/counter/index.js
+++ src/feature/counter/index.js
@@ -1,19 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect } from 'react';
 import { Card, CounterButton } from '../../components';
 import { divStyle } from './style';
 import useCounter from './hooks/useCounter';
 
 const CounterFeature = () => {
-
   const {
       counter,
       handleButton,
       getData,
-      ListData
+      ListData,
+      addToList
   } = useCounter();
 
   useEffect(()=>{
     getData();
+  },[]);
+
+  const handleBtnClick = useCallback((submitEvent)=>{
+    submitEvent.preventDefault();
+    submitEvent.target[0].value && addToList(submitEvent.target[0].value);
+    submitEvent.target[0].value = '';
   },[]);
 
   return (
@@ -28,8 +34,11 @@ const CounterFeature = () => {
         <select className='form-control'>
             {ListData}
         </select>
-        <input type='number' className='form-control my-2' />
-        <button className='btn btn-outline-primary my-2'> Add to List </button>
+
+        <form onSubmit={handleBtnClick}>
+          <input type='number' className='form-control my-2'/>
+          <input type='submit' className='btn btn-outline-primary my-2' value={'Add to List'}/>
+        </form>
      </Card>
      </>
   )
