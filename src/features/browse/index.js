41b2d2225355a248785f585147d0f70ed9e380a8
wajahatarajput <wajahatarajput@gmail.com>
2024-01-05 13:34:33 +0500

    hj

--- src/features/browse/index.js
+++ src/features/browse/index.js
@@ -1,28 +1,25 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react';
 import { PostCard, PostGrid } from '../../component';
 import { usePosts } from '../../hooks/usePosts';
 import { usePostProvider } from '../../providers';
 
 const BrowsePostsFeature = () => {
-
   const { getData, handleDelete } = usePosts();
   const { posts } = usePostProvider();
 
   useEffect(() => {
     getData();
-  }, [getData])
+  }, [getData]);
 
   const memoizedPostCards = useMemo(() => {
-    return posts?.map((post, index) => (
-      <PostCard key={index} post={post} handleDelete={handleDelete} index={index} />
-    ));
+    return (
+      posts?.map((post, index) => (
+        <PostCard key={index} post={post} handleDelete={handleDelete} index={index} />
+      )) || <p>No posts found</p>
+    );
   }, [posts, handleDelete]);
 
-  return (
-    <PostGrid>
-      {memoizedPostCards}
-    </PostGrid>
-  )
-}
+  return <PostGrid>{memoizedPostCards}</PostGrid>;
+};
 
 export default BrowsePostsFeature;
